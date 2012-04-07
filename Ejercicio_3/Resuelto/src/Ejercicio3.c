59f686c474fb186a78c1013c595a7860d5f820a0
Gaston Prieto <gaston.prieto.utn@gmail.com>
2012-04-07 16:28:48 -0300

    Agrego un loop en la preparacion
    Hago que las tareas se sigan ejecutando constantemente, asi le da un poco mas de realismo.

--- Ejercicio_3/Resuelto/src/Ejercicio3.c
+++ Ejercicio_3/Resuelto/src/Ejercicio3.c
@@ -1,206 +1,254 @@
 /*
- * Ejercicio3Resuelto.c
+ * Copyright (C) 2012 Sistemas Operativos - UTN FRBA. All rights reserved.
  *
- *  Created on: 10/03/2012
- *      Author: sebastian
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "Ejercicio3.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+static void Llegada_Nuevo_Cliente();
+static void Agregar_Pan_Inferior();
+static void Agregar_Condimento();
+static void Agregar_Carne();
+static void Agregar_Queso();
+static void Agregar_Panceta();
+static void Agregar_Lechuga();
+static void Agregar_Tomate();
+static void Agregar_Pan_Superior();
+static void Entregar();
+static void Agregar(t_Ingredientes ingrediente);
+
+static char* Ingrediente_ToString(t_Ingredientes ingrediente);
+static bool Esta_Bien_Armada();
+
 t_hamburguesa hamburgesa;
-sem_t semPanInferior;
-sem_t semCondimento;
-sem_t semCarne;
-sem_t semQueso;
-sem_t semPanceta;
-sem_t semLechuga;
-sem_t semTomate;
-sem_t semPanSuperior;
-sem_t semEntrega;
+pthread_mutex_t mutex;
 
-int main (void)
-{
-	pthread_t h1,h2,h3,h4,h5,h6,h7,h8;
+sem_t sem_nuevo_cliente;
+sem_t sem_pan_inferior;
+sem_t sem_condimento;
+sem_t sem_carne;
+sem_t sem_queso;
+sem_t sem_panceta;
+sem_t sem_lechuga;
+sem_t sem_tomate;
+sem_t sem_pan_superior;
+sem_t sem_entregar;
 
-	sem_init(&semPanInferior,0,1);
-	sem_init(&semCondimento,0,0);
-	sem_init(&semCarne,0,0);
-	sem_init(&semQueso,0,0);
-	sem_init(&semPanceta,0,0);
-	sem_init(&semLechuga,0,0);
-	sem_init(&semTomate,0,0);
-	sem_init(&semPanSuperior,0,0);
-	sem_init(&semEntrega,0,0);
+int main(void) {
+	pthread_t h0, h1, h2, h3, h4, h5, h6, h7, h8;
+	hamburgesa.embalada = false;
+	hamburgesa.ingredientes = queue_create();
 
-	hamburgesa.embalada=false;
-	hamburgesa.ingredientes=queue_create();
+	sem_init(&sem_nuevo_cliente,0,1);
+	sem_init(&sem_pan_inferior,0,0);
+	sem_init(&sem_condimento,0,0);
+	sem_init(&sem_carne,0,0);
+	sem_init(&sem_queso,0,0);
+	sem_init(&sem_panceta,0,0);
+	sem_init(&sem_lechuga,0,0);
+	sem_init(&sem_tomate,0,0);
+	sem_init(&sem_pan_superior,0,0);
+	sem_init(&sem_entregar,0,0);
 
-	pthread_create(&h1,NULL,Agregar_Pan_Inferior,NULL);
-	pthread_create(&h2,NULL,Agregar_Condimento,NULL);
-	pthread_create(&h3,NULL,Agregar_Carne,NULL);
-	pthread_create(&h4,NULL,Agregar_Queso,NULL);
-	pthread_create(&h5,NULL,Agregar_Panceta,NULL);
-	pthread_create(&h6,NULL,Agregar_Lechuga,NULL);
-	pthread_create(&h7,NULL,Agregar_Tomate,NULL);
-	pthread_create(&h7,NULL,Agregar_Pan_Superior,NULL);
-	pthread_create(&h8,NULL,Entregar,NULL);
+	pthread_create(&h0, NULL, (void*) Llegada_Nuevo_Cliente, NULL);
+	pthread_create(&h1, NULL, (void*) Agregar_Pan_Inferior, NULL);
+	pthread_create(&h2, NULL, (void*) Agregar_Condimento, NULL);
+	pthread_create(&h3, NULL, (void*) Agregar_Carne, NULL);
+	pthread_create(&h4, NULL, (void*) Agregar_Queso, NULL);
+	pthread_create(&h5, NULL, (void*) Agregar_Panceta, NULL);
+	pthread_create(&h6, NULL, (void*) Agregar_Lechuga, NULL);
+	pthread_create(&h7, NULL, (void*) Agregar_Tomate, NULL);
+	pthread_create(&h7, NULL, (void*) Agregar_Pan_Superior, NULL);
+	pthread_create(&h8, NULL, (void*) Entregar, NULL);
 
-	pthread_join(h1,(void**)NULL);
-	pthread_join(h2,(void**)NULL);
-	pthread_join(h3,(void**)NULL);
-	pthread_join(h4,(void**)NULL);
-	pthread_join(h5,(void**)NULL);
-	pthread_join(h6,(void**)NULL);
-	pthread_join(h7,(void**)NULL);
-	pthread_join(h8,(void**)NULL);
+	pthread_join(h1, (void**) NULL);
+	pthread_join(h2, (void**) NULL);
+	pthread_join(h3, (void**) NULL);
+	pthread_join(h4, (void**) NULL);
+	pthread_join(h5, (void**) NULL);
+	pthread_join(h6, (void**) NULL);
+	pthread_join(h7, (void**) NULL);
+	pthread_join(h8, (void**) NULL);
 
 	return EXIT_SUCCESS;
 }
 
-void*  Agregar_Pan_Inferior (void * args)
-{
-	sem_wait(&semPanInferior);
-	Agregar(PANINFERIOR);
-	sem_post(&semCondimento);
-	return NULL;
+static void Llegada_Nuevo_Cliente() {
+	while (1) {
+		sem_wait(&sem_nuevo_cliente);
+		printf("Esperando nuevo cliente...\n");
+		sleep(3);
+		printf("Lllego un nuevo cliente...\n");
+		sem_post(&sem_pan_inferior);
+	}
 }
 
-
-void*  Agregar_Condimento (void * args)
-{
-	sem_wait(&semCondimento);
-	Agregar(CONDIMENTOS);
-	sem_post(&semCarne);
-	return NULL;
+static void Agregar_Pan_Inferior() {
+	while (1) {
+		sem_wait(&sem_pan_inferior);
+		Agregar(PANINFERIOR);
+		sem_post(&sem_condimento);
+	}
 }
 
-void*  Agregar_Carne (void * args)
-{
-	sem_wait(&semCarne);
-	Agregar(CARNE);
-	sem_post(&semQueso);
-	return NULL;
+static void Agregar_Condimento() {
+	while (1) {
+		sem_wait(&sem_condimento);
+		Agregar(CONDIMENTOS);
+		sem_post(&sem_carne);
+	}
 }
 
-void*  Agregar_Queso (void * args)
-{
-	sem_wait(&semQueso);
-	Agregar(QUESO);
-	sem_post(&semPanceta);
-	return NULL;
+static void Agregar_Carne() {
+	while (1) {
+		sem_wait(&sem_carne);
+		Agregar(CARNE);
+		sem_post(&sem_queso);
+	}
 }
 
-void*  Agregar_Panceta (void * args)
-{
-	sem_wait(&semPanceta);
-	Agregar(PANCETA);
-	sem_post(&semLechuga);
-	return NULL;
+static void Agregar_Queso() {
+	while (1) {
+		sem_wait(&sem_queso);
+		Agregar(QUESO);
+		sem_post(&sem_panceta);
+	}
 }
 
-void*  Agregar_Lechuga (void * args)
-{
-	sem_wait(&semLechuga);
-	Agregar(LECHUGA);
-	sem_post(&semTomate);
-	return NULL;
+static void Agregar_Panceta() {
+	while (1) {
+		sem_wait(&sem_panceta);
+		Agregar(PANCETA);
+		sem_post(&sem_lechuga);
+	}
 }
 
-void*  Agregar_Tomate (void * args)
-{
-	sem_wait(&semTomate);
-	Agregar(TOMATE);
-	sem_post(&semPanSuperior);
-	return NULL;
+static void Agregar_Lechuga() {
+	while (1) {
+		sem_wait(&sem_lechuga);
+		Agregar(LECHUGA);
+		sem_post(&sem_tomate);
+	}
 }
 
-void*  Agregar_Pan_Superior (void * args)
-{
-	sem_wait(&semPanSuperior);
-	Agregar(PANSUPERIOR);
-	sem_post(&semEntrega);
-	sem_post(&semPanInferior);
-
-	return NULL;
+static void Agregar_Tomate() {
+	while (1) {
+		sem_wait(&sem_tomate);
+		Agregar(TOMATE);
+		sem_post(&sem_pan_superior);
+	}
 }
 
-
-
-void*  Entregar (void * args)
-{
-
-	sem_wait(&semEntrega);
-	if(!EstaBienArmada())
-		printf("Error en el armado de la hamburguesa!! El cliente se fue a comerse un triple bacon a mc :( \n");
-	else
-		printf("Hamburguesa armada perfectamente!! Le hemos tapado 5 arterias al cliente!! :)\n");
-	return NULL;
+static void Agregar_Pan_Superior() {
+	while (1) {
+		sem_wait(&sem_pan_superior);
+		Agregar(PANSUPERIOR);
+		sem_post(&sem_entregar);
+	}
 }
 
-void Agregar(t_Ingredientes ingrediente)
-{
-	printf("Agregando ingrediente %s\n", Ingrediente_ToString(ingrediente));
-	int * i= malloc(sizeof(int));
-	*i=ingrediente;
-	queue_push(hamburgesa.ingredientes,i);
-}
-
-int EstaBienArmada()
-{
-
-	for (int i=0; (((t_Ingredientes)i) != FIN) ; i++)
-	{
-		t_Ingredientes ingredienteCorrecto=(t_Ingredientes)i;
-		t_Ingredientes ingredienteHamburguesa;
-		int * ingrediente=queue_pop(hamburgesa.ingredientes);
-
-		if(ingrediente==NULL)
-		{
-			printf("Faltaron ingredientes!\n");
-			return 0;
+static void Entregar() {
+	while (1) {
+		sem_wait(&sem_entregar);
+		if (!Esta_Bien_Armada()) {
+			printf("Error en el armado de la hamburguesa!!\n");
+			printf("El cliente se fue a comerse un triple bacon a mc :(\n");
+		} else {
+			printf("Hamburguesa armada perfectamente!!\n");
+			printf("Le hemos tapado 5 arterias al cliente!! :)\n");
 		}
-		else
-		{
-			ingredienteHamburguesa=(t_Ingredientes)*ingrediente;
-			return ingredienteHamburguesa==ingredienteCorrecto;
+		sem_post(&sem_nuevo_cliente);
+	}
+}
+
+static void Agregar(t_Ingredientes ingrediente) {
+	pthread_mutex_lock(&mutex);
+	printf("Agregando ingrediente %s\n", Ingrediente_ToString(ingrediente));
+	int *i = malloc(sizeof(int));
+	*i = ingrediente;
+	queue_push(hamburgesa.ingredientes, i);
+	pthread_mutex_unlock(&mutex);
+}
+
+static bool Esta_Bien_Armada() {
+	/*Esta correctamente armada si tiene todos los ingredientes,
+	 *  y los tiene definidos en el orden dado por el enum t_Ingredientes*/
+
+	for (int i = 0; (((t_Ingredientes) i) <= PANSUPERIOR); i++) {
+		t_Ingredientes ingredienteCorrecto = (t_Ingredientes) i;
+		t_Ingredientes ingredienteHamburguesa;
+
+		pthread_mutex_lock(&mutex);
+		int* ingrediente = queue_pop(hamburgesa.ingredientes);
+		pthread_mutex_unlock(&mutex);
+
+		if (ingrediente == NULL) {
+			printf("Faltaron ingredientes!\n");
+			return false;
+		}
+
+		ingredienteHamburguesa = (t_Ingredientes) *ingrediente;
+		if (ingredienteHamburguesa != ingredienteCorrecto) {
+			printf("Ingrediente incorrecto debia ser %s y era %s\n",
+					Ingrediente_ToString(ingredienteCorrecto), Ingrediente_ToString(ingredienteHamburguesa));
+
+			free(ingrediente);
+			return false;
 		}
 	}
 
-	return 0;
+	return true;
 }
 
-char* Ingrediente_ToString(t_Ingredientes ingrediente)
-{
+static char* Ingrediente_ToString(t_Ingredientes ingrediente) {
 	char* ingredienteChar;
-	switch(ingrediente)
-	{
-		case PANINFERIOR:
-			ingredienteChar="PANINFERIOR";
-			break;
-		case PANSUPERIOR:
-				ingredienteChar="PANSUPERIOR";
-			break;
-		case CARNE:
-			ingredienteChar="CARNE";
-			break;
-		case QUESO:
-			ingredienteChar="QUESO";
-			break;
-		case PANCETA:
-			ingredienteChar="PANCETA";
-			break;
-		case LECHUGA:
-			ingredienteChar="LECHUGA";
-			break;
-		case TOMATE:
-			ingredienteChar="TOMATE";
-			break;
-		case CONDIMENTOS:
-			ingredienteChar="CONDIMENTOS";
-			break;
-		default:
-			ingredienteChar="Desconocido!";
-			break;
+	switch (ingrediente) {
+	case PANINFERIOR:
+		ingredienteChar = "PANINFERIOR";
+		break;
+	case PANSUPERIOR:
+		ingredienteChar = "PANSUPERIOR";
+		break;
+	case CARNE:
+		ingredienteChar = "CARNE";
+		break;
+	case QUESO:
+		ingredienteChar = "QUESO";
+		break;
+	case PANCETA:
+		ingredienteChar = "PANCETA";
+		break;
+	case LECHUGA:
+		ingredienteChar = "LECHUGA";
+		break;
+	case TOMATE:
+		ingredienteChar = "TOMATE";
+		break;
+	case CONDIMENTOS:
+		ingredienteChar = "CONDIMENTOS";
+		break;
+	default:
+		ingredienteChar = "Esto no debería ir en la hamburguesa!";
+		break;
 
 	}
 
--- Ejercicio_3/Resuelto/src/Ejercicio3.h
+++ Ejercicio_3/Resuelto/src/Ejercicio3.h
@@ -1,49 +1,38 @@
 /*
- * Ejercicio3.h
+ * Copyright (C) 2012 Sistemas Operativos - UTN FRBA. All rights reserved.
  *
- *  Created on: 10/03/2012
- *      Author: sebastian
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef EJERCICIO3_H_
 #define EJERCICIO3_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <stdbool.h>
-#include <semaphore.h>
 #include "Collections/queue.h"
 
 typedef struct {
 	bool embalada;
 	t_queue* ingredientes;
-}t_hamburguesa;
+} t_hamburguesa;
 
 typedef enum {
-	PANINFERIOR=0,
-	CONDIMENTOS=1,
-	CARNE=2,
-	QUESO=3,
-	PANCETA=4,
-	LECHUGA=5,
-	TOMATE=6,
-	PANSUPERIOR=7,
-	FIN=8
+	PANINFERIOR,
+	CONDIMENTOS,
+	CARNE,
+	QUESO,
+	PANCETA,
+	LECHUGA,
+	TOMATE,
+	PANSUPERIOR,
 } t_Ingredientes;
-
-void*  Agregar_Pan_Inferior (void * args);
-void*  Agregar_Condimento (void * args);
-void*  Agregar_Carne (void * args);
-void*  Agregar_Queso (void * args);
-void*  Agregar_Panceta (void * args);
-void*  Agregar_Lechuga (void * args);
-void*  Agregar_Tomate (void * args);
-void*  Agregar_Pan_Superior (void * args);
-void*  Entregar (void * args);
-void Agregar(t_Ingredientes ingrediente);
-char* Ingrediente_ToString(t_Ingredientes ingrediente);
-int EstaBienArmada();
 
 #endif /* EJERCICIO3_H_ */
